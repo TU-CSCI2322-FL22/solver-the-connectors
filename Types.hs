47d34b8894494127abc61183b89b29d4ae434a65
Mary Tish Phillips <marytishphillips@Marys-MacBook-Pro.local>
2022-10-31 14:54:33 -0500

    changes made by MTP --initial board added

--- Types.hs
+++ Types.hs
@@ -1,14 +1,21 @@
+module Board where
+import Data.List
+
 --Written by MTP below
 
-data Board = Board [Column]
+data Board = Board [Column] Heights deriving (Show, Eq) --[Columns] and list of lengths of the columns
+data Color = Red | Black | Empty deriving (Show, Eq )
+type Heights = [Int]
 type Column = [Spot]
 type Spot = (Coordinate, Color)
 type Coordinate = (Int, Int) -- (row, column)
-data Color = Red | Black | Empty deriving (Show, Eq, Ord)
+
 rows = 6
 columns = 7
 
-InitialBoard = Board [((x,y), Empty) | x <- [1..rows], y <- [1..columns]]
+InitialBoard = Board [[] | x <- [1..columns]] [0 | x <- [1..rows]]-- Board [[],[],[],[],[],[],[]] [0,0,0,0,0,0,0]
+
+
 
 --Written by MTP above
 
