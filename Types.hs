d9e9200e628db0be3c0587c21093c075b7153a49
Mary Tish Phillips <marytishphillips@Marys-MacBook-Pro.local>
2022-10-31 15:25:47 -0500

    Board functions MTP

--- Types.hs
+++ Types.hs
@@ -5,7 +5,7 @@ import Data.List
 
 data Board = Board [Column] Heights deriving (Show, Eq) --[Columns] and list of lengths of the columns
 data Color = Red | Black | Empty deriving (Show, Eq )
-type Heights = [Int]
+type Heights = [(Int, Int)]
 type Column = [Spot]
 type Spot = (Coordinate, Color)
 type Coordinate = (Int, Int) -- (row, column)
@@ -13,8 +13,16 @@ type Coordinate = (Int, Int) -- (row, column)
 rows = 6
 columns = 7
 
-InitialBoard = Board [[] | x <- [1..columns]] [0 | x <- [1..rows]]-- Board [[],[],[],[],[],[],[]] [0,0,0,0,0,0,0]
+InitialBoard = Board [[] | x <- [1..columns]] [(x,0) | x <- [1..rows]]-- Board [[],[],[],[],[],[],[]] [(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)]
 
+availableMoves :: Board -> [Int]
+availableMoves Board b h = [idx | (idx, cnt) <- h, cnt < row]
+
+addMove :: Board -> Int -> Board
+addMove (Board bd h) col = 
+    let hghts [if (idx == col) then (idx, cnt+1) else (idx, cnt)| (idx,cnt) <- h]
+        bord = --[if(col == idx) then ]
+    in Board bord hghts
 
 
 --Written by MTP above
