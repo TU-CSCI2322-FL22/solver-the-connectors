244f49bd0cfbf2293fad17801c12094c7e38147e
Mary Tish Phillips <marytishphillips@marys-mbp.trinity.local>
2022-10-31 16:11:50 -0500

    milestone 1 notes added and function stubs added

--- Types.hs
+++ Types.hs
@@ -1,6 +1,8 @@
 module Board where
 import Data.List
 
+------------------------------------------MILESTONE ONE-----------------------------------------------
+
 --Written by MTP below
 
 data Board = Board [Column] Heights deriving (Show, Eq) --[Columns] and list of lengths of the columns
@@ -24,6 +26,19 @@ addMove (Board bd h) col =
         bord = --[if(col == idx) then ]
     in Board bord hghts
 
+winnerRow :: Board -> (Color, Bool)
+winnerRow = undefined
+
+winnerColumn :: Board -> (Color, Bool)
+winnerColumn = undefined
+
+winnerDiagonal :: Board -> (Color, Bool)
+winnerDiagonal = undefined 
+
+isWinner :: Board -> (Color, Bool) --returns the Color and True if there is a a winner, so that we know which player won. 
+isWinner bd = snd (winnerColumn bd) | snd (winnerRow bd) | snd (winnerDiagonal bd)
 
 --Written by MTP above
 
+--We will additonally need "A pretty show function for a game state, to ease debugging."
+--Full Credit: All of these functions should consider possible errors or edge cases: what if there no winner, what if the move is not legal for the current game, etc. Use Maybe's or Either's appropriately.
