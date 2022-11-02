6f492830bf8615d8a167a13525535063b2ec1fdb
2021-08-24 <adeerin2@janus24.cs.trinity.edu>
2022-11-02 12:59:10 -0500

    Dr.Fogarty

--- Types.hs
+++ Types.hs
@@ -7,17 +7,18 @@ the board game, and tell if a player has won the board game. -}
 
 --Written by MTP below
 
-data Board = Board [Column] Heights deriving (Show, Eq) --[Columns] and list of lengths of the columns
+data Board = Board [Column] Color deriving (Show, Eq) --[Columns] and list of lengths of the columns
 --create an instance of Show for Board to show the current game state
 data Color = Red | Black deriving (Show, Eq)
-instance Show Color where
-    show (Red) = "0"
-    show(Black) = "X"
+showcolor (Red) = "0"
+showColor (Black) = "X"
     
-type Heights = [(Int, Int)]
-type Column = [Spot]
-type Spot = (Coordinate, Color)
-type Coordinate = (Int, Int) -- (row, column)
+--type Heights = [(Int, Int)]
+type Column = (Int,[Color])
+
+--type Spot = (Coordinate, Color)
+--type Coordinate = (Int, Int) -- (row, column)
+type Move = Int
 
 rows = 6
 columns = 7
@@ -32,7 +33,7 @@ availableMoves Board b h = [idx | (idx, cnt) <- h, cnt < row]
 
 updateBoard :: Board -> Int -> Int -> Board
 updateBoard (Board (x:xs) hg) ct cl = if ct == cl then (,):x
-addMove :: Board -> Int -> Board
+addMove :: Board -> Int -> Maybe Board
 addMove (Board bd h) col = 
     let hghts [if (idx == col) then (idx, cnt+1) else (idx, cnt)| (idx,cnt) <- h]
         bord = --[if(col == idx) then ]
@@ -47,9 +48,10 @@ winnerColumn = undefined
 winnerDiagonal :: Board -> (Color, Bool)
 winnerDiagonal = undefined 
 
-isWinner :: Board -> (Color, Bool) --returns the Color and True if there is a a winner, so that we know which player won. 
+isWinner :: Board -> Maybe Color --returns the Color and True if there is a a winner, so that we know which player won. 
 isWinner bd = snd (winnerColumn bd) | snd (winnerRow bd) | snd (winnerDiagonal bd)
-
+--Data type for outcome to check if tie
+--
 --Written by MTP above
 
 --We will additonally need "A pretty show function for a game state, to ease debugging."
