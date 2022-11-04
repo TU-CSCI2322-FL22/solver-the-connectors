7358c9bf041a1478a5625a4374f73a7ff1bba096
Audrey Deering <audreydeering@audreys-air.trinity.local>
2022-11-03 21:31:49 -0500

    Finished the checkWinner function and cleaned up the other winner-related functions- Audrey Deering

--- Types.hs
+++ Types.hs
@@ -18,14 +18,16 @@ type Column = [Color]
 --list of colors in that column
 --with the item at index zero in [Color] being the piece at the "bottom"
 --To find the row number, we'd use the piece's index in [Color]
-type Piece = ((Int,Int), Color)
---This represents a piece, with it's coordinates (row, column) and color
 type Move = Int
 --which column the move is going into
 type State = (Board, Color)
 --state of the board, where the color represents the winner if there is one 
 -- A little superfluous but we'll see.
-type Coordinat3e = (Int, Int)
+type Winner = (Bool, Color)
+type Coordinate = (Int, Int)
+type Direction = (Int, Int)
+
+
 
 showcolor (Red) = "0"
 showColor (Black) = "X"
@@ -50,17 +52,25 @@ availableMoves brd =
         aux :: Board -> [Move] -> Int -> [Move]
         aux (Board [] clr) lst cnt = lst
         aux (Board (c:cs) clr) lst cnt =  
-            if length c < rows then aux (Board cs clr) (cnt:lst) (cnt+1) 
+            if length c < rows 
+            then aux (Board cs clr) (cnt:lst) (cnt+1) 
             else aux (Board cs clr) lst (cnt+1)
             --also switched out 6 for rows to make it more abstract (?)
 
 --was pattern matched but we tweakin
---make sure when you call updateBoard passing in a 0 for count initially 
+--make sure when you call updateBoard passing in a 0 for count initially
+--I feel like to updateBoard/makeMove we onlY need Board and Move as inputss 
 updateBoard :: Board -> Move -> Int -> [Column] -> Board
 updateBoard (Board (x:xs) clr) mv cnt accumlst = 
-    let newcolor = if clr == Red then Black else Red
-    in if (cnt == mv) then (Board (accumlst ++ (clr:x):xs) newcolor)
-       else updateBoard (Board xs clr) mv (cnt+1) (x:accumlst)
+    let 
+        newcolor = 
+            if clr == Red 
+            then Black 
+            else Red
+    in 
+        if (cnt == mv) 
+        then (Board (accumlst ++ (clr:x):xs) newcolor)
+        else updateBoard (Board xs clr) mv (cnt+1) (x:accumlst)
 
 ub = updateBoard (Board [[Red, Black, Red, Black, Red, Black], [Red, Black, Black, Black, Red], [Black, Red, Black]] Red) 1 0 []
 {-updateBoard b@(Board (x:xs) clr) mv =
@@ -87,91 +97,72 @@ makeMove brd mv =
 --if move `elem` availableMoves brd then else return error
 --If we check if it's a valid move before, do we need a Maybe Board? TBH I still don't really understand
 --Maybe. I think it would elimate the need for updateBoard, but they're kind of going to do the same thing
-{-winnerRow :: Board -> (Color, Bool)
-winnerRow = undefined
 
-winnerColumn :: Board -> (Color, Bool)
-winnerColumn (Board [] colr) = (Neither, False) 
-winnerColumn (Board (b:bs) colr) = 
-    let helperresult = aux b;
-    in if (snd (helperresult) == True) then helperresult else winnerColumn (Board bs colr)
-    where aux lst = 
-                let grouped = group lst
-                    fourcolor = [head x | x <- grouped, (length x) >= 4]
-                in if fourcolor == [] then (Neither, False) else (head fourcolor, True)
 
-winnerDiagonalAsc :: Board -> (Color, Bool)
-winnerDiagonalAsc = undefined 
-
-winnerDiagonalDes :: Board -> (Color, Bool)
-winnerDiagonalDes = undefined -}
-
-{-getColumn :: Board -> Move -> Int
+--Takes a board and a move (column number) and returns the column
+--returns [] if the column is empty or out of bounds
+--otherwise it returns a column
+getColumn :: Board -> Move -> Column
 getColumn (Board cols clr) mv = 
         let
-            drC = drop (m - 1) cols
+            drC = drop (mv - 1) cols
         in 
             if drC == [] then [] else head drC
 
-getCoordinate :: Board -> Move -> (Int, Int)
-getCoordinate (Board col clr) mv = (last getColumn ((Board cols clr) mv) , mv) --(Int, Int)
-
---If no color is in that position, or if that position is out of bounds, it will return []
-findColor :: Board -> (Int, Int) -> Color
-findColor (Board bd clr) (x,y) = 
-    let 
-        drC = drop (y - 1) bd
-        col = if drC == [] then [] else head drC
-        beforein = if col == [] then Neither else drop (x - 1) col
-    in 
-        if beforein == Neither 
-        then Neither 
-        else (if drop(x-1) col == [] then Neither else head (drop (x - 1) col))
--}
-
-{-leftAsc :: Board -> Color -> (Int, Int) -> Int -> Int --Board -> Color -> Move -> Count ->4 FinalCount
-leftAsc (Board cols clr) mv 4 = 4
-leftAsc (Board cols clr) mv cnt = undefined
-
-rightAsc :: Board -> Color -> (Int, Int) -> Int -> Int
-rightAsc (Board cols clr) mv 4 = 4
-rightAsc (Board cols clr) mv cnt = undefined
-
-leftDsc :: Board -> Color -> (Int, Int) -> Int -> Int --Board -> Color -> Move -> Count ->4 FinalCount
-leftDsc (Board cols clr) mv 4 = 4
-leftDsc (Board cols clr) mv cnt = undefined
-
-rightDsc :: Board -> Color -> (Int, Int) -> Int -> Int
-rightDsc (Board cols clr) c (row, col) 4 = 4
-rightDsc (Board cols clr) c (row, col) cnt = 
+--findColor takes a Board and a Coordinate and finds the Color at that coordinate
+--If there is no color in the coordinate, or if that position is out of bounds, it will return neither
+--NOTE: Our makeMove function must prevent out of bound moves
+--(This doesn't check if it's out of bounds, it's just saying that if their is not a color in that
+--position it must be either empty or out of bounds)
+findColor :: Board -> Coordinate -> Color
+findColor (Board cols clr) (x,y) = 
     let
-        nextPosition = (row - 1, col + 1)
-        newCol = findColor (Board cols clr) nextPosition
+        col = getColumn (Board cols clr) y
     in
-        if newCol == c
-        then 1 + rightDsc (Board cols clr) c nextPosition cnt 
+        if col == []
+        then error "Not a valid coordinate! No column! (Either move hasn't been made here or out of bounds)"
+        else getColorAtRow col x
+    where
+        getColorAtRow :: Column -> Int -> Color
+        getColorAtRow cl r
+            |drop (r-1) cl == [] = error "Not a valid coordinate! No row! (Either move hasn't been made here or out of bounds)"
+            |otherwise = head (drop (r-1) cl)
 
-down :: Board -> Color -> (Int, Int) -> Int -> Int
-down (Board cols clr) mv 4 = 4 
-down (Board cols clr) mv cnt = 
+    
+--takes board, Color we're checking for, coord of piece we're "at", direction we're going in, and a count
+--returns a count of how many in a row of the color were checking there is in that Direction
+countDir :: Board -> Color -> Coordinate -> Direction -> Int -> Int
+countDir (Board cols cl) cChecking (row, col) (mvR, mvC) cnt =
+    let
+        nextPos = (row + mvR, col + mvC)
+        nextPosCol = findColor (Board cols cl) nextPos
+    in
+        if nextPosCol == cChecking
+        then countDir (Board cols cl) cChecking nextPos (mvR, mvC) (cnt + 1)
+        else cnt
 
-right :: Board -> Color -> (Int, Int) -> Int -> Int
-right (Board cols clr) mv 4 = 4 
-right (Board cols clr) mv cnt = 
 
-left :: Board -> Color -> (Int, Int) -> Int -> Int
-left (Board cols clr) mv 4 = 4 
-left (Board cols clr) mv cnt = 
+checkWinner :: Board -> Color -> Coordinate -> Winner
+checkWinner (Board cols cl) cChecking (row, col) =
+    let
+        --I don't like having all this repeated but I didn't want to deal with an
+        --association list when trying to add the directions together
+        lftAsc = countDir (Board cols cl) cChecking (row, col) (1,-1) 0
+        rtAsc = countDir (Board cols cl) cChecking (row, col) (1,1) 0
+        lft = countDir (Board cols cl) cChecking (row, col) (0,-1) 0
+        rt = countDir (Board cols cl) cChecking (row, col) (0,1) 0
+        lftDsc = countDir (Board cols cl) cChecking (row, col) (-1,-1) 0
+        dw = countDir (Board cols cl) cChecking (row, col) (-1, 0) 0
+        rtDsc = countDir (Board cols cl) cChecking (row, col) (-1,1) 0
 
-
-
-
-isWinner :: Board -> Move -> Maybe Color --returns the Color and True if there is a a winner, so that we know which player won. 
-isWinner (Board cols clr) mv =  
-    let newcolor = --snd (winnerColumn bd) | snd (winnerRow bd) | snd (winnerDiagonalAsc bd) | snd (winnerDiagonalDes bd)
---Data type for outcome to check if tie
---
---Written by MTP above
+        fstDiag = lftAsc + 1 + rtDsc
+        sndDiag = rtAsc + 1 + lftDsc
+        horz = lft + 1 + rt
+        vert = dw + 1
+    in  
+        if fstDiag > 3 || sndDiag > 3 || horz > 3 || vert > 3
+        then (True, cChecking)
+        else (False, Neither)
 
 --We will additonally need "A pretty show function for a game state, to ease debugging."
 --Full Credit: All of these functions should consider possible errors or edge cases: what if there no winner, what if the move is not legal for the current game, etc. Use Maybe's or Either's appropriately.--
@@ -179,4 +170,3 @@ isWinner (Board cols clr) mv =
 --TESTER CODE--
 --testWC = winnerColumn (Board [[Red, Black, Red, Black, Black, Red], [Red, Black, Black, Black, Black, Red], [Black, Red, Black, Red, Red, Black]] Red) --WORKS
 testAM = availableMoves (Board [[Red, Black, Red, Black, Red, Black], [Red, Black, Black, Black, Red], [Black, Red, Black]] Red) --WORKS
--}
